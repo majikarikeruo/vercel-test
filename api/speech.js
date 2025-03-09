2698eee787c353668375371bff17c760add5b122
majikarikeruo <castero1219@gmail.com>
2025-03-09 19:02:42 +0900

    [mdify]書き方を追加

--- api/speech.js
+++ api/speech.js
@@ -1,21 +1,19 @@
 //1つ目の引数がフロントからデータを受け取る固有の場所になっている
-export default async function GET(request) {
+export default async function handler(request) {
   //バックエンドのルート定義
   console.log(1, request.params);
   console.log(2, request.query);
 
   try {
     // URLSearchParamsを使って、検索用のクエリパラメータ（speaker、from、until）を設定
-    const params = new URLSearchParams({
-      speaker: request.query.speaker,
-      from: request.query.from,
-      until: request.query.until,
-    });
-    console.log(3, params);
+    const { searchParams } = new URL(request.url);
+    const speaker = searchParams.get("speaker");
+    const from = searchParams.get("from");
+    const until = searchParams.get("until");
+
+    console.log(3, speaker, from, until);
     //APIにリクエストを送信
-    const response = await fetch(
-      `https://kokkai.ndl.go.jp/api/speech?speaker=${request.query.speaker}&from=${request.query.from}&until=${request.query.until}&recordPacking=json`
-    );
+    const response = await fetch(`https://kokkai.ndl.go.jp/api/speech?speaker=${speaker}&from=${from}&until=${until}&recordPacking=json`);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
@@ -28,6 +26,7 @@ export default async function GET(request) {
       headers: {
         "content-type": "application/json",
       },
+      timeout: 30000,
     });
   } catch (error) {
     console.error("Error fetching data:", error);
